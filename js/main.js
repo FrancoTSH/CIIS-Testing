ca9ca09452b0ec301c3ad01c2d46e0303543a0dc
Franco <58120780+FrancoTSH@users.noreply.github.com>
2020-11-11 18:58:37 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -19,7 +19,10 @@
         submenu.classList.contains('active') ? document.querySelector('div.nav-link svg path').setAttribute("d", "M112 328l144-144 144 144"): document.querySelector('div.nav-link svg path').setAttribute("d", "M112 184l144 144 144-144");
     });
 
-    document.querySelector('.video-selector').addEventListener('click', () => trailer.classList.add('active'));
+    document.querySelector('.video-selector').addEventListener('click', () => {
+        trailer.querySelector('source').setAttribute("src","assets/videos/video_ciis.mp4");
+        trailer.classList.add('active');
+    });
     document.querySelector('.modal-video .close').addEventListener('click', () => {
         trailer.classList.remove('active');
         player.stop();
